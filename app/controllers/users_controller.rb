7cf0fcf5851f814492f891442aea30614245d004
Aya Hamdy <124109149+ayahamdy22@users.noreply.github.com>
2025-05-30 20:37:33 +0000

    Added UsersController with index action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,11 @@
+# class UsersController < ApplicationController
+#   def show
+#     @users = "finally! the buzzle is done."
+#   end
+# end
+
 class UsersController < ApplicationController
-  def show
-    @users = "finally! the buzzle is done."
+  def index
+    render plain: "Users page is working!"
   end
 end
